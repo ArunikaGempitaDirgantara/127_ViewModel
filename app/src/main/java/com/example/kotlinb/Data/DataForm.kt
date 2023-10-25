37738ce3330ea7d9d70e899f4532341704a2fd72
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2023-10-25 22:31:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kotlinb/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinb.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/kotlinb/Data/datasource.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinb.Data
+
+import com.example.kotlinb.R
+
+object datasource {
+    val jenis = listOf(
+        R.string.JK_1,
+        R.string.JK_2
+    )
+}
